819e3a32a836d8a772bb913d2053d4db9eee79df
oyamasiphula <oyama@projectcodex.co>
2017-03-08 23:17:36 +0200

    added more functionality to the exercise page using DOM and addEventListeners

--- js/greet/greet.client.js
+++ js/greet/greet.client.js
@@ -1,12 +1,20 @@
 var okButton = document.getElementById('okButton');
-var nameField = document.getElementById('specifyName2ndVer');
+var nameFieldInput = document.getElementById('specifyName');
+var backButtonElement = document.querySelector("#backButton");
 
-var getName2ndVer = function() {
-    var name = nameField.value;
+var getNameVal = function() {
+    var name = nameFieldInput.value;
     if (name.trim().length === 0) {
         return;
     }
-    document.getElementById('specifyName2ndVerPlaceHolder').innerHTML = greet(name);
-    nameField.value = '';
+    document.getElementById('speciedNameDisplay').innerHTML = greet(name);
+    nameFieldInput.value = '';
 };
-okButton.addEventListener('click', getName2ndVer)
+var strLink = 'file:///home/oyama/projects/Codex_Repo/bootCamp_workouts_CS/index.html';
+
+var goBackToMain = function() {
+    window.location.href = strLink;
+}
+
+okButton.addEventListener('click', getNameVal)
+backButton.addEventListener('click', goBackToMain)
