d3835d5bc6a32843b8a7b131e2088e9bdc3c5c51
oyamasiphula <oyama@projectcodex.co>
2017-03-10 09:32:56 +0200

    added conditionals in order to greet with a specific language

--- js/greet/greet.js
+++ js/greet/greet.js
@@ -1,4 +1,20 @@
-var greet = function(name){
-	var greetings = "Hello, " + name;
-	return greetings;
+var greet = function(language,name) {
+
+    var Sotho_greet = "Dumela, " + name;
+    var Xhosa_greet = "Molo, " + name;
+    var English_greet = "Hello, " + name;
+
+    if ("Sotho" === language) {
+        return Sotho_greet;
+    };
+    if ("Xhosa" === language) {
+        return Xhosa_greet;
+    };
+    if ("English" === language) {
+        return English_greet;
+    };
+};
+
+var getName = function(name) {
+    return name;
 };
