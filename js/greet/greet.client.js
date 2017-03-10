0933b5c437731199cc9d022415782ff8265a85e3
oyamasiphula <oyama@projectcodex.co>
2017-03-10 09:32:56 +0200

    added radio functionality to target my values

--- js/greet/greet.client.js
+++ js/greet/greet.client.js
@@ -1,20 +1,37 @@
 var okButton = document.getElementById('okButton');
 var nameFieldInput = document.getElementById('specifyName');
+var radios = document.getElementsByName('languages');
 var backButtonElement = document.querySelector("#backButton");
 
+
+var count = 0;
+
 var getNameVal = function() {
+
     var name = nameFieldInput.value;
     if (name.trim().length === 0) {
         return;
     }
-    document.getElementById('speciedNameDisplay').innerHTML = greet(name);
-    nameFieldInput.value = '';
-};
-var strLink = 'file:///home/oyama/projects/Codex_Repo/bootCamp_workouts_CS/index.html';
+    for (var i = 0, collLen = radios.length; i < collLen; i++) {
 
+        var selectedLanguage = radios[i].value
+        var checked = radios[i].checked;
+
+        if (!checked) {
+            //skip the code below go to the next loop item
+            continue;
+        }
+        document.getElementById('speciedNameDisplay').innerHTML = greet(selectedLanguage, getName(name));
+        count++;
+        document.querySelector("#totalNumOfGreetings").innerHTML = count;
+        nameFieldInput.value = '';
+    }
+};
+
+var strLink = 'file:///home/oyama/projects/Codex_Repo/bootCamp_workouts_CS/index.html';
 var goBackToMain = function() {
     window.location.href = strLink;
 }
 
-okButton.addEventListener('click', getNameVal)
-backButton.addEventListener('click', goBackToMain)
+okButton.addEventListener('click', getNameVal);
+backButton.addEventListener('click', goBackToMain);
