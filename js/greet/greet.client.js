f7ae83f2a875437cdc597190a3c2e25370002444
oyamasiphula <oyama@projectcodex.co>
2017-03-14 12:34:24 +0200

    now added a fully functional code for my radios,text input and buttons

--- js/greet/greet.client.js
+++ js/greet/greet.client.js
@@ -1,15 +1,20 @@
 var okButton = document.getElementById('okButton');
-var nameFieldInput = document.getElementById('specifyName');
-var radios = document.getElementsByName('languages');
-var backButtonElement = document.querySelector("#backButton");
+    nameFieldInput = document.getElementById('specifyName'),
+    radios = document.getElementsByName('languages'),
+    backButtonElement = document.querySelector("#backButton"),
+    resetButton = document.querySelector(".button-primary");
 
-var count = 0;
-var myStorage = localStorage;
-var count = localStorage.getItem('count');
+var count = 0,
+    myStorage = localStorage,
+    count = myStorage.getItem('count');
+
 var getNameVal = function() {
 
     var name = nameFieldInput.value;
-    if (name.trim().length === 0) {
+
+    if (name.trim().length === 0 || name.trim().length < 4 ) {
+      nameFieldInput.value = '';
+      window.alert("Please enter correct name");
         return;
     }
     for (var i = 0, collLen = radios.length; i < collLen; i++) {
@@ -33,6 +38,13 @@ var strLink = 'file:///home/oyama/projects/Codex_Repo/bootCamp_workouts_CS/index
 var goBackToMain = function() {
     window.location.href = strLink;
 }
-
+var resetCounter = function(){
+  window.myStorage.removeItem("count");
+  count = 0;
+  window.myStorage.setItem("count",count)
+  document.querySelector("#totalNumOfGreetings").innerHTML = count;
+  console.log(myStorage.count);
+}
 okButton.addEventListener('click', getNameVal);
+resetButton.addEventListener('click', resetCounter);
 backButton.addEventListener('click', goBackToMain);
