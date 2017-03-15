04ab89c758caafb15e17a2b2f2a630e416878d08
oyamasiphula <oyama@projectcodex.co>
2017-03-15 12:39:18 +0200

    now added more test cases for my greet function

--- test/tests.js
+++ test/tests.js
@@ -3,6 +3,8 @@ var assert = chai.assert;
 describe("Test for all the client JS code", function() {
     it("Should test for greet function", function() {
 
-        assert.equal(greet("Janine"), "Hello, Janine");
+        assert.equal(greet("English","Janine"), "Hello, Janine");
+        assert.equal(greet("Xhosa","Janine"), "Molo, Janine");
+        assert.equal(greet("Sotho","Janine"), "Dumela, Janine");
     });
 });
