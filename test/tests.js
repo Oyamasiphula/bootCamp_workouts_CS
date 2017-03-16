a3bfa2aa68fa036ffe1da8fec4722603edb58338
oyamasiphula <oyama@projectcodex.co>
2017-03-16 22:53:24 +0200

    added more specific tests for each specific function file

--- test/tests.js
+++ test/tests.js
@@ -7,4 +7,20 @@ describe("Test for all the client JS code", function() {
         assert.equal(greet("Xhosa","Janine"), "Molo, Janine");
         assert.equal(greet("Sotho","Janine"), "Dumela, Janine");
     });
+    it("Should test for which day function", function() {
+        assert.equal(dayDetails('Saturday') , true);
+    });
+    it("Should test for multiplyThem function", function() {
+        assert.equal( multiplyThem(2, 3) , 6);
+        assert.equal(multiplyThem(15,5), 75);
+    });
+    it("Should test for favouriteSport function", function() {
+        assert.equal(favSport(), "David Blankit's favourate sport is Golf");
+    });
+    it("Should test for isFromBellville function", function() {
+        assert.equal(favSport(), "David Blankit's favourate sport is Golf");
+    });
+    it("Should test for isFromBellville function", function() {
+        assert.equal(favSport(), "David Blankit's favourate sport is Golf");
+    });
 });
