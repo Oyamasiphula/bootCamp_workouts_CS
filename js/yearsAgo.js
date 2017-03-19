b04666435695c9ed3a4dab66e0052869c8442218
oyamasiphula <oyama@projectcodex.co>
2017-03-19 22:44:16 +0200

    added function that returns range as the difference in years

--- js/yearsAgo.js
+++ js/yearsAgo.js
@@ -1,4 +1,9 @@
 var yearsAgo = function(year) {
-    let range = new Date().getFullYear("12-12-2017") - new Date(year).getFullYear();
+    var range = 2017 - year;
+    return range;
+}
+
+var yearsAgo2ndVer = function(year) {
+    var range = new Date().getFullYear("12-12-2017") - new Date(year).getFullYear();
     return range;
 }
