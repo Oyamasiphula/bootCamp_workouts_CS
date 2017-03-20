85aeaa6175b2252ad90c0757a6d1190921f0c84e
oyamasiphula <oyama@projectcodex.co>
2017-03-20 04:36:05 +0200

    added function that returns the place of transport based registration param value

---
+++ js/fromWhere.js
@@ -0,0 +1,13 @@
+var fromWhere = function(regNumber) {
+    if (regNumber.startsWith("CY")) {
+        return "Bellville";
+    }
+    if (regNumber.startsWith("CJ")) {
+        return "Paarl";
+    }
+    if (regNumber.startsWith("CA")) {
+        return 'Cape Town';
+    } else {
+        return "Some other place!";
+    }
+}
