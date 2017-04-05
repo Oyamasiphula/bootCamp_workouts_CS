740a445f3cfdfe9e63175ff89f00272615744eb5
oyamasiphula <oyama@projectcodex.co>
2017-04-05 12:30:47 +0200

    added filter function to filter through items(Shoes)(good state)

--- js/shoe_catalogue/shoeCatalogue.js
+++ js/shoe_catalogue/shoeCatalogue.js
@@ -1,15 +1,26 @@
 var optionTemplate = document.querySelector(".options");
 var messageTemplate = document.querySelector(".message");
-var divAsOutput = document.querySelector(".dataListContainer");
+var dataListDivAsOutput = document.querySelector(".dataListContainer");
+var messageDivAsOutput = document.querySelector(".messageContainer");
+var searchResultsDiv = document.querySelector(".searchResults");
 var messageOut = document.querySelector("h4.messageOut");
+var dataSearchedTemplate = document.querySelector(".searchedDataSummary");
 
 var shoes = [{
+        brand: 'Nike',
         color: 'blue',
         price: 350,
         size: 4,
         in_stock: 5
+    },{
+        brand: 'Puma',
+        color: 'blue',
+        price: 350,
+        size: 8,
+        in_stock: 18
     },
     {
+        brand: 'Adidas',
         color: 'orange',
         price: 275,
         size: 7,
@@ -17,15 +28,18 @@ var shoes = [{
     }
 ];
 
-var colorColl = [];
-var sizesColl = [];
-var stockColl = [];
 
 (function() {
+
+  var colorColl = [];
+  var sizesColl = [];
+  var stockColl = [];
+
     for (var i = 0; i < shoes.length; i++) {
 
         var color = shoes[i].color;
         var sizes = shoes[i].size;
+        var prices = shoes[i].price;
         var stock = shoes[i].in_stock;
 
         if (shoes[i].color) {
@@ -34,25 +48,49 @@ var stockColl = [];
         if (shoes[i].size) {
             sizesColl.push(sizes);
         }
-        if (shoes[i].in_stock) {
-            stockColl.push(stock);
-        }
     }
+    var sizeColorTempHelpersRes = Handlebars.compile(optionTemplate.innerHTML);
+    var messageOutResHelperRes = Handlebars.compile(messageTemplate.innerHTML);
+
+    var humanReadableSizeAndColorOutput = sizeColorTempHelpersRes({
+      color: colorColl,
+      sizes: sizesColl
+    });
+
+    var humanReadableForm = messageOutResHelperRes({
+      NoOfShoesAvail: stockColl
+    });
+
+    dataListDivAsOutput.innerHTML = humanReadableSizeAndColorOutput;
+    messageDivAsOutput.innerHTML = humanReadableForm;
 })();
 
-var SizeColorTempHelpersRes = Handlebars.compile(optionTemplate.innerHTML);
-var messageOutResHelperRes  = Handlebars.compile(messageTemplate.innerHTML);
 
-var humanReadableSizeAndColorOutput = SizeColorTempHelpersRes({
-   color: colorColl,
-   sizes: sizesColl
-});
+var tableResultTemplate = Handlebars.compile(dataSearchedTemplate.innerHTML);
 
-var humanReadableForm = messageOutResHelperRes({
-   NoOfShoesAvail: stockColl
-});
+var selectColorOpt = document.querySelector(".color");
+var selectSizeOpt = document.querySelector(".sizes");
 
+var filterItems = function() {
+    var capturedData = [];
 
-console.log(humanReadableForm);
-divAsOutput.innerHTML = humanReadableSizeAndColorOutput;
-messageOut = humanReadableForm;
+    let selectedColorOpt = selectColorOpt[selectColorOpt.selectedIndex].value;
+    let selectedSizeOpt = selectSizeOpt[selectSizeOpt.selectedIndex].value;
+
+    for (var i = 0; i < shoes.length; i++) {
+        var color = shoes[i].color;
+        var brands = shoes[i].brand;
+        var sizes = shoes[i].size;
+
+        if (selectedColorOpt === color && Number(selectedSizeOpt) === sizes) {
+            capturedData.push(shoes[i]);
+        }
+    }
+    var writeTableHelpersResult = tableResultTemplate({
+        dataSearched: capturedData
+    });
+    searchResultsDiv.innerHTML = writeTableHelpersResult;
+}
+
+var searchButton = document.querySelector(".searchButton");
+searchButton.addEventListener("click", filterItems)
