222cf3a00536079c7253087b0e675ca61bcd1e6e
oyamasiphula <oyama@projectcodex.co>
2017-05-17 08:19:11 +0200

    added behaviour for matching days

---
+++ js/matching_days/matchingClient.js
@@ -0,0 +1,30 @@
+var days = document.querySelectorAll(".day"),
+    date1 = document.querySelector(".date1"),
+    date2 = document.querySelector(".date2");
+
+function matchingDays(dateInputs) {
+  dateInputs.addEventListener("change", function() {
+      for (var i = 0; i < days.length; i++) {
+      var date1Val = new Date(date1.value),
+          date1day = date1Val.getDay();
+
+      var date2Val = new Date(date2.value),
+          date2day = date2Val.getDay();
+
+          if (date1day === Number(days[i].dataset.weekId)) {
+            days[i].style.backgroundColor = "blue";
+          }
+          if (date2day === Number(days[i].dataset.weekId)) {
+            days[i].style.backgroundColor = "red";
+          }
+          if (date1day === Number(days[i].dataset.weekId) && date2day === Number(days[i].dataset.weekId)) {
+              days[i].style.backgroundColor = "green";
+          }
+          if (!date1day === Number(days[i].dataset.weekId)) {
+            days[i].style.backgroundColor = "";
+          }
+      };
+  });
+}
+matchingDays(date1);
+matchingDays(date2);
