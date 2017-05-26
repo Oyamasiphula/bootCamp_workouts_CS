83589da5314b79abf6424b0be5a730b77d5557b4
oyamasiphula <oyama@projectcodex.co>
2017-05-26 19:57:59 +0200

    addded the function that returns a validates and stops the execution process if the user entry is empty

---
+++ js/registration_number/isValidReg.js
@@ -0,0 +1,7 @@
+var validateInputAndParseCorrectValue = function(inputValueTextBoxVal){
+  if (inputValueTextBoxVal.trim().length === 0 || typeof(inputValueTextBoxVal) === Number) {
+      alert("Please enter a valid regstration !");
+      return 
+  }
+  return inputValueTextBoxVal;
+}
