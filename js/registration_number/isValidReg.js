1ae85c376bf91cb18cd38c62e7353d5f7fa0c738
oyamasiphula <oyama@projectcodex.co>
2017-05-27 22:40:06 +0200

    added fully working validation code

--- js/registration_number/isValidReg.js
+++ js/registration_number/isValidReg.js
@@ -1,7 +1,7 @@
 var validateInputAndParseCorrectValue = function(inputValueTextBoxVal){
   if (inputValueTextBoxVal.trim().length === 0 || typeof(inputValueTextBoxVal) === Number) {
       alert("Please enter a valid regstration !");
-      return 
+      return
   }
   return inputValueTextBoxVal;
 }
