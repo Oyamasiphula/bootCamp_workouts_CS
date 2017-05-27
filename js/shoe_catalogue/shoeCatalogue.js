de80fbc2cca03f953aceec1bf2ec1ea6201b9bf4
oyamasiphula <oyama@projectcodex.co>
2017-05-27 22:40:06 +0200

    removed and reused function createUnqueList code

--- js/shoe_catalogue/shoeCatalogue.js
+++ js/shoe_catalogue/shoeCatalogue.js
@@ -132,23 +132,7 @@ var shoes = [{
 var newList = shoes;
 
 function onBrowserLoadEl() {
-    var createUniqList = function(list, key) {
-        var organizedShoePropColl = [],
-            organizedShoePropMap = {};
 
-        for (var i = 0; i < list.length; i++) {
-            var obj = list[i];
-            var val = obj[key];
-
-            if (organizedShoePropMap[val] === undefined) {
-                organizedShoePropMap[val] = val;
-                organizedShoePropColl.push({
-                    [key]: val
-                });
-            }
-        };
-        return organizedShoePropColl;
-    }
     var organizedBrandColl = createUniqList(newList, "brand"),
         organizedColorColl = createUniqList(newList, "color"),
         organizedSizeColl = createUniqList(newList, "size");
