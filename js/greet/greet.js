6815b583d15ff449a4a900184db4bdcea136b30a
oyamasiphula <oyama@projectcodex.co>
2017-05-30 11:52:18 +0200

    added local storage for the count var

--- js/greet/greet.js
+++ js/greet/greet.js
@@ -1,6 +1,28 @@
-var greet = function(language, name) {
+var count = 0,
+    myStorage = localStorage,
+    count = myStorage.getItem('count');
 
-    var properNameForm = name.charAt(0).toUpperCase() + name.substring(1)
+
+var getName = function(name) {
+    var properNameForm = name.charAt(0).toUpperCase() + name.substring(1);
+
+    var personGreeted = {};
+
+    if (personGreeted.name === undefined) {
+        personGreeted["name"] = properNameForm;
+        personGreeted["count"] = count++;
+    } 
+
+    console.log(myStorage.count, personGreeted);
+
+    return personGreeted;
+};
+
+
+var greet = function(language, nameObj) {
+console.log(nameObj["name"]);
+
+    var properNameForm = nameObj["name"].charAt(0).toUpperCase() + nameObj["name"].substring(1)
 
     var Sotho_greet = "Dumela, " + properNameForm;
     var Xhosa_greet = "Molo, " + properNameForm;
@@ -15,16 +37,4 @@ var greet = function(language, name) {
     if ("English" === language) {
         return English_greet;
     };
-};
-var getName = function(name) {
-    var properNameForm = name.charAt(0).toUpperCase() + name.substring(1);
-
-    var personGreeted = {};
-
-    if (personGreeted["name"] === undefined) {
-        personGreeted["name"] = properNameForm;
-    }
-    // window.alert(personGreeted.name);
-
-    return personGreeted;
 };
