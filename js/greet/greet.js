d7965e56a0e023a166e40d9cc77bdbe6b5d51664
oyamasiphula <oyama@projectcodex.co>
2017-06-01 23:04:52 +0200

    added greet function

--- js/greet/greet.js
+++ js/greet/greet.js
@@ -1,32 +1,7 @@
-var count = 0,
-    myStorage = localStorage,
-    count = myStorage.getItem('count');
-
-
-var getName = function(name) {
-    var properNameForm = name.charAt(0).toUpperCase() + name.substring(1);
-
-    var personGreeted = {};
-
-    if (personGreeted.name === undefined) {
-        personGreeted["name"] = properNameForm;
-        personGreeted["count"] = count++;
-    } 
-
-    console.log(myStorage.count, personGreeted);
-
-    return personGreeted;
-};
-
-
 var greet = function(language, nameObj) {
-console.log(nameObj["name"]);
-
-    var properNameForm = nameObj["name"].charAt(0).toUpperCase() + nameObj["name"].substring(1)
-
-    var Sotho_greet = "Dumela, " + properNameForm;
-    var Xhosa_greet = "Molo, " + properNameForm;
-    var English_greet = "Hello, " + properNameForm;
+    var Sotho_greet = "Dumela, " + personGreeted.name;
+    var Xhosa_greet = "Molo, " + personGreeted.name;
+    var English_greet = "Hello, " + personGreeted.name;
 
     if ("Sotho" === language) {
         return Sotho_greet;
@@ -38,3 +13,10 @@ console.log(nameObj["name"]);
         return English_greet;
     };
 };
+
+
+// getName()
+// getName()
+// getName()
+// getName()
+// getName()
