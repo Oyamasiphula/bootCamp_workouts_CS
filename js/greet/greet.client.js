ac4e8531cc65397bbe946b51246134bcf3792bd1
oyamasiphula <oyama@projectcodex.co>
2017-06-08 09:42:10 +0200

    added more functionality on the submit button

--- js/greet/greet.client.js
+++ js/greet/greet.client.js
@@ -1,10 +1,10 @@
-var getNameVal = function(name) {
-
-    if (name.trim().length === 0 || name.trim().length < 4) {
-        nameFieldInput.value = '';
-        window.alert("Please enter correct name");
-        return;
-    }
+var getNameValidation = function(name) {
+  
+  if (name.trim().length === 0 || name.trim().length < 4) {
+      nameFieldInput.value = '';
+      window.alert("Please enter correct name");
+      return;
+  }
     for (var i = 0, collLen = radios.length; i < collLen; i++) {
 
         var selectedLanguage = radios[i].value
@@ -14,9 +14,9 @@ var getNameVal = function(name) {
             //skip the code below go to the next loop item
             continue;
         }
-        printOutPut("specificNameDisplay", greet(selectedLanguage, getNameProperties.greetOnePerson));
-        printOutPut("totalNumOfGreetings", "You have been greeted " + getNameProperties.count + " Times");
-        nameFieldInput.value = '';
+        printOutPut("specificNameDisplay", greet(selectedLanguage, name));
+        let nameKeyVal = name;
+        printOutPut("totalNumOfGreetings", "You have been greeted " + factoryFunction.all()[nameKeyVal] + " Times");
     }
 };
 
@@ -27,7 +27,11 @@ var goBackToMain = function() {
 
 okButton.addEventListener('click', function(e) {
     e.preventDefault;
-    factoryFunction.greetOnePerson(getNameVal(nameFieldInput.value));
+    let actualPersonsName = getNameVal(nameFieldInput.value);
+    factoryFunction.greetOnePerson(actualPersonsName);
+    getNameValidation(getNameVal(nameFieldInput.value))
+    nameFieldInput.value = '';
+
 });
 resetButton.addEventListener('click', resetCounter);
 backButton.addEventListener('click', goBackToMain);
