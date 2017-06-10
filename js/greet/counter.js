784fb96fd2fad67dc810b2ac4496c8163c536666
oyamasiphula <oyama@projectcodex.co>
2017-06-11 01:36:35 +0200

    added name validation before inserting name value pair check its length and if inappropriate delete it (functionality)

--- js/greet/counter.js
+++ js/greet/counter.js
@@ -3,27 +3,32 @@ var counter = function(people) {
     var peopleGreeted = {};
     if (people) {
         peopleGreeted = people;
-    }
+    };
 
     let greetOnePerson = function(name) {
         if (peopleGreeted[name] === undefined) {
             peopleGreeted[name] = 0;
         }
         peopleGreeted[name] += 1;
-    }
+    };
     let greetedCount = function() {
         var list = [];
         for (people in peopleGreeted) {
             list.push(people);
         }
         return list.length;
-    }
+    };
     let timesGreeted = function(name) {
         return peopleGreeted[name];
-    }
+    };
     let greetedPeopleObj = function() {
+        for (var name in peopleGreeted) {
+            if (name.length < 3) {
+                delete peopleGreeted[name];
+            }
+        };
         return peopleGreeted;
-    }
+    };
 
     return {
         greetOnePerson,
